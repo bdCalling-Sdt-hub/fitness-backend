42a8899319d61c63dc0db4bfe9ea21ebf1bae2d8
Yeasin Arafat <leoarafat10@gmail.com>
2024-08-02 12:14:35 +0600

    refactor file uploader

--- src/app.ts
+++ src/app.ts
@@ -20,13 +20,14 @@ app.use(
       'http://192.168.30.250:3002',
       'http://192.168.10.102:3001',
       'http://192.168.30.250:3008',
+      'http://192.168.10.147:3001',
     ],
     credentials: true,
   }),
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '3000mb' }));
+app.use(express.urlencoded({ extended: true, limit: '3000mb' }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
